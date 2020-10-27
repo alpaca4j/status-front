69a36c4a20298b850fb23fa95fc4df42765d6b10
Marcin Badura <marcin@badura.io>
2020-10-27 20:05:26 +0100

    time fix

--- src/app/services/status.service.ts
+++ src/app/services/status.service.ts
@@ -1,19 +1,26 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Status} from "../models/Status";
-import {Observable} from "rxjs";
+import {Observable, pipe} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatusService {
   // backendUrl:string= 'http://localhost:8080/status?scope=test';
-  backendUrl:string= '/api/status?scope=test';
+  backendUrl: string = '/api/status?scope=test';
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
-  getStatuses():Observable<Status[]> {
-    return this.http.get<Status[]>(this.backendUrl)
+  getStatuses(): Observable<Status[]> {
+    return this.http.get<Status[]>(this.backendUrl).pipe(
+      map(data => data
+        .map(v => {
+          v.checkDate = new Date(v.checkDate);
+          return v;
+        })))
+
   }
 }
